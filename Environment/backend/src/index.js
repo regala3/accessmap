34fe3ec573f7f9aa8549652c665c59ec636599c9
regala3 <161763238+regala3@users.noreply.github.com>
2025-11-26 12:22:53 -0800

    changed index.js

--- Environment/backend/src/index.js
+++ Environment/backend/src/index.js
@@ -20,11 +20,29 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use(cookieParser());
-app.use(cors({
-  origin: FRONTEND_URL,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+
+const allowedOrigins = [
+  'http://localhost:5173', // development
+  'https://accessiblemap.org', //Production
+  'https://www.accessiblemap.org' // production with www
+];
+
+const corsOption = {
+  origin: function(origin, callback) {
+    if(!origin) return callback(null, true);
+
+    if(allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }, 
   credentials: true,
-}));
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedOrigins: ["Content-Type", "Authorization"]
+};
+
+app.use(cors(corsOption));
 
 console.log(FRONTEND_URL);
 app.use("/api/auth", authRoutes);
