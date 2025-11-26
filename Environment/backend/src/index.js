e7d2a177fa168fc3a52f27000f5a2cb7d1492af1
regala3 <161763238+regala3@users.noreply.github.com>
2025-11-26 01:13:59 -0800

    added allowedHeaders to Cors

--- Environment/backend/src/index.js
+++ Environment/backend/src/index.js
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders:["Content-Type", "Authorization"],
   credentials: true,
 }));
 
